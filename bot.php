63901247c26926436979bf12cfb4fef0f525a3ad
puarns <puarns@gmail.com>
2017-03-06 20:08:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,9 +23,24 @@ if (!is_null($events['events'])) {
 					'text' => 'Condo Parkland เพชรเกษม-ท่าพระ ห้อง 1835 ชั้น 18 เลขที่ 99/657 ถนน เพชรเกษม แขวงวัดท่าพระเขตบางกอกใหญ่ กรุงเทพมหานคร 10600'
 				];
 			} else {
+				$url = 'http://linebot.linetor.com/api.php';
+				$data = array('mid' => '123', 'message' => $message);
+				$content = json_encode($data);
+				$curl = curl_init($url);
+				curl_setopt($curl, CURLOPT_URL, $url);
+				//curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+				curl_setopt($curl, CURLOPT_POST, 1);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+				//curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 20);
+				$result = curl_exec($curl);
+				$response = json_decode($result, true);
+				$returnMessage = $response['msg'];
+				
 				$messages = [
 					'type' => 'text',
-					'text' => $text
+					'text' => $returnMessage
 				];
 			}
 
