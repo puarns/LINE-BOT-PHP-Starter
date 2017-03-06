9631981ea4b4c0e9cae9eff822667c5d45d23f82
puarns <puarns@gmail.com>
2017-03-06 20:36:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,46 +15,12 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			
-			// Build message to reply back
-			/*$header = array('Content-Type: application/json');
-			$url = 'http://linebot.linetor.com/api.php';
-			$data = array('mid' => '123', 'message' => $text);
-			$content = json_encode($data);
-			$curl = curl_init($url);
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-			curl_setopt($curl, CURLOPT_POST, 1);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-			//curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 20);
-			$result = curl_exec($curl);
-			$response = json_decode($result, true);
-			$returnMessage = $response['msg'];*/
-			$returnMessage = 'ddda';
-			
-			/*$data = [
-				'mid' => '123',
-				'messages' => $text
-			];
-			$post = json_encode($data);
-			$url = 'http://linebot.linetor.com/api.php';
-			$headers = array('Content-Type: application/json');
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			$response = json_decode($result, true);
-			$returnMessage = $response['msg'];
 
+			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $returnMessage
-			];*/
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
