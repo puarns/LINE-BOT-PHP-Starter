d181df23a2c342a99e984590b7692a5ed1475093
puarns <puarns@gmail.com>
2017-06-06 15:31:16 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -12,8 +12,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 
 $result = curl_exec($ch);
 curl_close($ch);
